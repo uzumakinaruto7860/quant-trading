fbdcbc69096ef1d7215826e4bcef455f77f8be2d
harismehboob142 <harismehboob2019@gmail.com>
2022-11-06 18:59:34 +0500

    refined ticker

--- Trading-Track/src/components/Ticker.jsx
+++ Trading-Track/src/components/Ticker.jsx
@@ -4,7 +4,29 @@ import { TickerTape } from "react-ts-tradingview-widgets";
 const Ticker = () => {
     return (
         <div>
-            <TickerTape colorTheme="dark" isTransparent={true}></TickerTape>
+            <TickerTape colorTheme="dark" isTransparent={true} showSymbolLogo={true} largeChartUrl={false}
+                symbols={[
+                    {
+                        "proName": "FX_IDC:PKRUSD",
+                        "title": "PKR/USD"
+                    },
+                    {
+                        "proName": "FOREXCOM:NSXUSD",
+                        "title": "Nasdaq 100"
+                    },
+                    {
+                        "proName": "FX_IDC:EURUSD",
+                        "title": "EUR/USD"
+                    },
+                    {
+                        "proName": "BITSTAMP:BTCUSD",
+                        "title": "BTC/USD"
+                    },
+                    {
+                        "proName": "BITSTAMP:ETHUSD",
+                        "title": "ETH/USD"
+                    }
+                ]} />
         </div>
     )
 }
