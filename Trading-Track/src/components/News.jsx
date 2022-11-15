67ed4c4278c4a431446c223157ca1c48c8b5587b
harismehboob142 <harismehboob2019@gmail.com>
2022-11-15 15:05:39 +0500

    struggling with news API

--- Trading-Track/src/components/News.jsx
+++ Trading-Track/src/components/News.jsx
@@ -1,62 +1,71 @@
 import styles, { layout } from "../style";
-import { MarketOverview } from "react-ts-tradingview-widgets";
+import { useEffect } from "react";
+import { useState } from "react";
 
 
-const News = () => (
+const News = () => {
 
-    <>
+    const [news, setNews] = useState([])
+    const response = {};
+    useEffect(() => {
 
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'f4693e8cacmshf1cb4bdb462146bp121510jsne55367585024',
+                'X-RapidAPI-Host': 'seeking-alpha.p.rapidapi.com'
+            }
+        };
 
+        fetch('https://seeking-alpha.p.rapidapi.com/news/v2/list-trending?until=0&since=0&size=20', options)
+            .then(response => response.json())
+            .then(response => setNews(response.data))
+            .catch(err => console.error(err));
 
+    }, [])
+    // console.log(news[0].attributes.title);
 
-        <section id="product" className={layout.sectionReverse}>
-            <div className={layout.sectionImgReverse}>
+    return (
+        <>
+            <section id="product" className={layout.sectionReverse}>
+                <div className={layout.sectionImgReverse}>
 
-                <div className="flex flex-col bg-white m-auto p-auto bg-transparent mt-20">
+                    <div className="flex flex-col bg-white m-auto p-auto bg-transparent mt-20">
 
-                    <div
-                        className="flex overflow-x-scroll pb-10 w-[600px] h-[300px]"
-                    >
                         <div
-                            className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
+                            className="flex overflow-x-scroll pb-10 w-[600px] h-[300px]"
                         >
-                            <div className="inline-block px-3">
-                                <div
-                                    className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-green-400 hover:shadow-xl transition-shadow duration-300 ease-in-out"
-                                ></div>
-                            </div>
-                            <div className="inline-block px-3">
-                                <div
-                                    className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-red-400 hover:shadow-xl transition-shadow duration-300 ease-in-out"
-                                ></div>
-                            </div>
-                            <div className="inline-block px-3">
-                                <div
-                                    className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-blue-400 hover:shadow-xl transition-shadow duration-300 ease-in-out"
-                                ></div>
-                            </div>
-                            <div className="inline-block px-3">
-                                <div
-                                    className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-purple-400 hover:shadow-xl transition-shadow duration-300 ease-in-out"
-                                >Last card</div>
+                            <div
+                                className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
+                            >
+                                {news.map((eachNews) => {
+                                    console.log(eachNews.attributes.titles)
+                                    return <div className="inline-block px-3">
+                                        <div
+                                            className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-green-400 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                                        >{eachNews.attributes.titles}</div>
+                                    </div>
+                                })}
+
+
                             </div>
                         </div>
                     </div>
+
                 </div>
 
-            </div>
+                <div className={layout.sectionInfo}>
+                    <h2 className={styles.heading2}>
+                        Get market Overview on <br className="sm:block hidden" /> snap of your fingers.
+                    </h2>
+                    <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                        Maket Overview plays a role in deciding the volatility of market. Track the news to properly handle your trades and get rid of high volatility times for a low risk of capital liquidity
+                    </p>
 
-            <div className={layout.sectionInfo}>
-                <h2 className={styles.heading2}>
-                    Get market Overview on <br className="sm:block hidden" /> snap of your fingers.
-                </h2>
-                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-                    Maket Overview plays a role in deciding the volatility of market. Track the news to properly handle your trades and get rid of high volatility times for a low risk of capital liquidity
-                </p>
-
-            </div>
-        </section>
-    </>
-);
+                </div>
+            </section>
+        </>
+    )
+};
 
 export default News;
