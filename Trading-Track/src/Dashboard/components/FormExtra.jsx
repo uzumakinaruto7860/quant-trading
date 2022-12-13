c2a23b2e6eebcbca56b7593c5f16af0f84681465
harismehboob142 <harismehboob2019@gmail.com>
2022-12-13 19:16:02 +0500

    login form completed

--- Trading-Track/src/Dashboard/components/FormExtra.jsx
+++ Trading-Track/src/Dashboard/components/FormExtra.jsx
@@ -1,7 +1,24 @@
 export default function FormExtra() {
     return (
-        <>
+        <div className="flex items-center justify-between ">
+            <div className="flex items-center">
+                <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-white focus:ring-purple-500 border-gray-300 rounded"
+                />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-white">
+                    Remember me
+                </label>
+            </div>
 
-        </>
+            <div className="text-sm">
+                <a href="#" className="font-medium text-purple-600 hover:text-purple-500">
+                    Forgot your password?
+                </a>
+            </div>
+        </div>
+
     )
 }
