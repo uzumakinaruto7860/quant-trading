2769efbfe36a8806a572e6c1c0bafde16d6905e3
harismehboob142 <harismehboob2019@gmail.com>
2022-12-13 19:21:45 +0500

    done finalizing color of login page

--- Trading-Track/src/Dashboard/components/Header.jsx
+++ Trading-Track/src/Dashboard/components/Header.jsx
@@ -14,12 +14,12 @@ export default function Header({
                     className="h-14 w-14"
                     src="https://ik.imagekit.io/pibjyepn7p9/Lilac_Navy_Simple_Line_Business_Logo_CGktk8RHK.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649962071315" />
             </div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-500">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-400">
                 {heading}
             </h2>
             <p className="mt-2 text-center text-sm text-gray-500 mt-5">
                 {paragraph} {' '}
-                <Link to={linkUrl} className="font-medium text-purple-600 hover:text-purple-500">
+                <Link to={linkUrl} className="font-medium text-purple-500 hover:text-purple-500">
                     {linkName}
                 </Link>
             </p>
