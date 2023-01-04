9eba2deaea4caea133f5b35428e5028870df6d43
Bilal <malixbilix786@gmail.com>
2023-01-03 23:05:47 -0800

    pips difference

--- Trading-Track/src/Dashboard/MainDashboard/src/components/Add_Trade.jsx
+++ Trading-Track/src/Dashboard/MainDashboard/src/components/Add_Trade.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Add_Trade = () => {
-    return (
-        <div className=" bg-white py-10 px-5 m-auto w-full h-full ">
 
-            <div className="text-3xl mb-20 text-center">
+    const [open, setOpen] = useState(0.0);
+    const [close, setClose] = useState(0.0);
+
+    const handleOpenOnChange = (e) => {
+        setOpen(e.target.value);
+        console.log(open)
+    }
+    const handleCloseOnChange = (e) => {
+        setClose(e.target.value);
+        console.log(close);
+    }
+
+
+    return (
+        <div className=" bg-blue-100 py-10 px-5 m-auto w-full h-full ">
+
+            <div className="text-3xl mb-20 mt-7 text-center">
                 Add New Trade
             </div>
 
             <div className="grid grid-cols-2 gap-4 max-w-2xl m-auto">
 
                 <div className="col-span-2 lg:col-span-1 mx-4">
-                    <input type="text" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Pair Name" />
+                    <input type="text" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-lg" placeholder="Pair Name" />
                 </div>
 
                 <div className="col-span-2 lg:col-span-1 px-3 mx-4 my-4">
@@ -19,7 +34,7 @@ const Add_Trade = () => {
 
                     <label for="action" className='px-3 p-3'>Buy/Sell</label>
 
-                    <select name="action" id="cars">
+                    <select name="action" id="action" className='rounded-lg'>
                         <option value="volvo">Buy</option>
                         <option value="saab">Sell</option>
                     </select>
@@ -27,27 +42,32 @@ const Add_Trade = () => {
                 </div>
 
                 <div className="col-span-2 lg:col-span-1 mx-4 my-4">
-                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Open Price" />
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-lg" placeholder="Open Price" onChange={handleOpenOnChange} />
                 </div>
 
                 <div className="col-span-2 lg:col-span-1 mx-4 my-4">
-                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Lot Size" />
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-lg" placeholder="Lot Size" />
                 </div>
 
                 <div className="col-span-2 lg:col-span-1 mx-4 my-4">
-                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Close Price" />
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-lg" placeholder="Close Price" onChange={handleCloseOnChange} />
                 </div>
 
                 <div className="col-span-2 lg:col-span-1 mx-4 my-4">
-                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Timeframe" />
+                    <input type="text" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-lg" placeholder="Timeframe" />
                 </div>
 
                 <div className="col-span-2 lg:col-span-1 mx-4 my-4">
-                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="$ P/L" />
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-lg" placeholder="$ P/L" />
                 </div>
+
+                <div className="col-span-2 lg:col-span-1 mx-4 my-4">
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-lg" disabled value={(close - open).toPrecision(4)} placeholder="Lots Difference" />
+                </div>
+
 
                 <div className="col-span-2 text-right">
-                    <button className="py-3 px-6 bg-indigo-500 hover:bg-indigo-600 text-white font-bold w-full sm:w-32">
+                    <button className="py-3 px-6 bg-indigo-500 hover:bg-indigo-600 text-white font-bold w-full sm:w-32 rounded-lg">
                         Submit
                     </button>
                 </div>
