a56715334d93bd0cd6c384f34b474ffffff77ce6
Bilal <malixbilix786@gmail.com>
2023-01-03 22:39:10 -0800

    added add_trade form

--- Trading-Track/src/Dashboard/MainDashboard/src/components/Add_Trade.jsx
+++ Trading-Track/src/Dashboard/MainDashboard/src/components/Add_Trade.jsx
@@ -1,49 +1,57 @@
 import React from 'react'
-import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 const Add_Trade = () => {
-    var [articles, setArticles] = useState([])
-    const API_KEY = '26c92b41998e4059a32348fb598c07ac';
-    const CURRENCY = 'USD';
-
-    function fetchNews() {
-        axios.get(`https://newsapi.org/v2/everything?q=${CURRENCY}&sortBy=relevancy&apiKey=${API_KEY}`)
-            .then(response => {
-                setArticles(response.data.articles);
-                console.log(response.data.articles)
-                // Now you can use the "articles" array to display the news in your React component
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
-
-    useEffect(() => {
-        fetchNews();
-
-    }, [])
-
-
-
-
-
     return (
-        <div className='bg-black'>
-            <div className="flex flex-wrap">
-                {articles.map(article => (
-                    <div className="w-1/2 md:w-1/3 lg:w-1/4 p-2" key={articles.url}>
-                        <div className="bg-white rounded-lg shadow-md overflow-hidden">
-                            <img src={article.urlToImage} alt={article.title} className="w-full h-48 object-cover" />
-                            <div className="p-4">
-                                <h3 className="font-bold text-lg mb-2">{article.title}</h3>
-                                <p className="text-gray-700 text-sm">{article.description}</p>
-                                <a href={article.url} className="block mt-4 text-blue-500 hover:text-blue-700">Read more</a>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+        <div className=" bg-white py-10 px-5 m-auto w-full h-full ">
+
+            <div className="text-3xl mb-20 text-center">
+                Add New Trade
+            </div>
+
+            <div className="grid grid-cols-2 gap-4 max-w-2xl m-auto">
+
+                <div className="col-span-2 lg:col-span-1 mx-4">
+                    <input type="text" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Pair Name" />
+                </div>
+
+                <div className="col-span-2 lg:col-span-1 px-3 mx-4 my-4">
+                    {/* <input type="text" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Email Address"/> */}
+
+                    <label for="action" className='px-3 p-3'>Buy/Sell</label>
+
+                    <select name="action" id="cars">
+                        <option value="volvo">Buy</option>
+                        <option value="saab">Sell</option>
+                    </select>
+
+                </div>
+
+                <div className="col-span-2 lg:col-span-1 mx-4 my-4">
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Open Price" />
+                </div>
+
+                <div className="col-span-2 lg:col-span-1 mx-4 my-4">
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Lot Size" />
+                </div>
+
+                <div className="col-span-2 lg:col-span-1 mx-4 my-4">
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Close Price" />
+                </div>
+
+                <div className="col-span-2 lg:col-span-1 mx-4 my-4">
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Timeframe" />
+                </div>
+
+                <div className="col-span-2 lg:col-span-1 mx-4 my-4">
+                    <input type="number" className=" border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="$ P/L" />
+                </div>
+
+                <div className="col-span-2 text-right">
+                    <button className="py-3 px-6 bg-indigo-500 hover:bg-indigo-600 text-white font-bold w-full sm:w-32">
+                        Submit
+                    </button>
+                </div>
+
             </div>
         </div>
     )
